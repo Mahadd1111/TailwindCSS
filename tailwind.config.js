95c0faba09dc8677e5b7f23eb61f148d2355e880
Mahad Ahmed <mahadddd1111@gmail.com>
2022-07-07 20:12:45 +0500

    Tailwind Practice

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/code.js",
+    "./public/index.html",
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
